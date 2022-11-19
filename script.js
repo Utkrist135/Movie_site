a798f47e70e95735717c9ed6fdb421944badc5dd
Ut135 <91059203+Utkrist135@users.noreply.github.com>
2022-11-19 08:48:27 +0000

    Created a skeleton of the website

---
+++ script.js
@@ -0,0 +1,3 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5e20c4a14944d97051022e023c740bf5&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=5e20c4a14944d97051022e023c740bf5&query=";
